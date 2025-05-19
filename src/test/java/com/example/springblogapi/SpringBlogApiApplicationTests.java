bd7e4e1ede7640b8831f8c95294fde085e9ba6ba
nihadhuseynll <huseynlinihad4400@gmail.com>
2025-05-19 10:21:47 +0400

    Initial commit

---
+++ src/test/java/com/example/springblogapi/SpringBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springblogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
